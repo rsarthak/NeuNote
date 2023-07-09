1eed471af223302e646bda4c109c460e18d2c0bc
Sarthak Rajput <60438849+rsarthak@users.noreply.github.com>
2023-07-09 21:35:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect} from 'react'
 import {nanoid} from 'nanoid'
 import NotesList from './components/notesList'
 import Header from './components/header';
